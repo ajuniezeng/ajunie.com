5d6f0766c63fea560d8e65d141ba5ca6fee6a13a
Ajunie Zeng <ajuniezeng@gmail.com>
2025-08-18 19:58:39 -0700

    feat: add rss feeds

---
+++ src/pages/rss-cn.xml.ts
@@ -0,0 +1,27 @@
+import { SITE } from '@/constants'
+import { useTranslations } from '@/i18n/utils';
+import { getPosts } from '@/lib/utils'
+import rss from '@astrojs/rss'
+import type { APIContext } from 'astro'
+
+export async function GET(context: APIContext) {
+  try {
+    const posts = await getPosts('cn');
+    const translation = useTranslations('cn');
+
+    return rss({
+      title: SITE.TITLE,
+      description: translation('layout.defaultDescription'),
+      site: context.site ?? SITE.SITE_URL,
+      items: posts.map((post) => ({
+        title: post.data.title,
+        description: post.data.description,
+        pubDate: post.data.date,
+        link: `/cn/articles/${post.id}/`,
+      })),
+    })
+  } catch (error) {
+    console.error('Error generating RSS feed:', error)
+    return new Response('Error generating RSS feed', { status: 500 })
+  }
+}
